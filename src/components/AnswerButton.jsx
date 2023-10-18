6e3c090c6a36994fad11d6daefa0272c4241a580
lkf6214 <lkf6214@naver.com>
2023-10-18 22:57:51 +0900

    add: answer button

---
+++ src/components/AnswerButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AnswerButton({ text }) {
+  return (
+    <>
+      <div className="border-[#E5E5E5] borfer-[1px] rounded-lg"> {text} </div>
+    </>
+  );
+}
